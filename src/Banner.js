8dcb162fc58a25b16f60663853450796ab3c0017
min-jungeun <mje971115@gmail.com>
2024-07-09 11:42:05 +0900

    스와이퍼 자동 롤링 3초 진행

--- src/Banner.js
+++ src/Banner.js
@@ -1,27 +1,30 @@
-
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css'; 
 import datainfo from './data/db.json';
+
 
 const Banner = () => {
     return (
         <Swiper
             spaceBetween={50}
-            slidesPerView={3}
+            slidesPerView={1}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
-    >
-                {
-                    datainfo.banner.map((v, i)=>{
-                        return(
-                            <SwiperSlide className={v.cls}>{v.atext}</SwiperSlide>
-                        )
-                    })
-                    
-                }
-                
-                
-    </Swiper>
-    )
+            modules={[Autoplay, Pagination, Navigation]}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+            }}
+        >
+            {
+              datainfo.banner.map((v, i) => (
+                  <SwiperSlide key={i} className={v.cls}>{v.atext}</SwiperSlide>
+              ))
+            }
+        </Swiper>
+    );
 }
 
-export default Banner
+export default Banner;
