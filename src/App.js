76c41f875023901295cdaf40b8a2b337c566da85
min-jungeun <mje971115@gmail.com>
2024-07-10 11:47:35 +0900

    스타일컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Title , Abtn} from './commonui/ui'
+import {Title , Abtn, Button, Buttoncolor, Buttonany} from './commonui/ui'
 
 
 function App() {
@@ -16,6 +16,10 @@ function App() {
         나는 타이틀입니다
         <Abtn>응모하기</Abtn>
       </h2>
+      <Button>나는 일반 버튼</Button>
+      <Button primary>나는 일반 버튼</Button>
+      <Buttoncolor bgcolor="red" textbgcolor="white">난 배경색과 글자색 너가 결정해</Buttoncolor>
+      <Buttonany bgcolor="black" textbgcolor="white" fontSize="100px" fontfmily="맑은 고딕">fontfmily는 삼항식으로</Buttonany>
      
     </div>
   );
--- src/commonui/ui.js
+++ src/commonui/ui.js
@@ -18,3 +18,39 @@ export const Abtn = styled.a`
     color:white;
   }
 `;
+
+export const Button = styled.button`
+  background: ${props => props.primary ? 'palevioletred' : 'white'};
+  color: ${props => props.primary ? 'white' : 'palevioletred'};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  &:hover {
+    background: ${props => props.primary ? 'white' : 'palevioletred'};
+    color: ${props => props.primary ? 'palevioletred' : 'white'};
+  }
+`;
+
+export const Buttoncolor = styled.button`
+  background: ${props => props.bgcolor};
+  color: ${props => props.textbgcolor};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+export const Buttonany = styled.button`
+  background: ${props => props.bgcolor};
+  color: ${props => props.textbgcolor};
+  font-size: ${props => props.fontSize};
+  font-family: ${props => props.fontfmily}
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
